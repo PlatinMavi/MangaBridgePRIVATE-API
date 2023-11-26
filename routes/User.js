e8aa809d3a3807e990de12028bf37303c4044a18
Alper Gezer <a.yz07x@gmail.com>
2023-11-26 15:52:15 +0300

    Cookie SameSite fix

--- routes/User.js
+++ routes/User.js
@@ -1,60 +1,73 @@
-const router = require("express").Router()
-const bcrypt = require("bcrypt")
-const jwt = require("jsonwebtoken")
-const mongoose = require("mongoose")
-const UserModel = require("../models/User.model")
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const UserModel = require("../models/User.model");
 
-const JWTsecret = process.env.JWT_SECRET
+const JWTsecret = process.env.JWT_SECRET;
 
-router.post('/login', async (req,res) => {
-    const {username,password} = req.body;
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
 
-    try{
-      const userDoc = await UserModel.findOne({username});
-      const passOk = bcrypt.compareSync(password, userDoc.password);
-      if (passOk) {
-        // logged in
-        jwt.sign({username,id:userDoc._id}, JWTsecret, {}, (err,token) => {
-          if (err) throw err;
-          res.cookie('token', token).json({
-            id:userDoc._id,
-            username,
-          });
-        });
-      } else {
-        res.status(400).json('wrong credentials');
-      }
-    } catch(e){
-      res.status(400).json("error")
+    try {
+        const userDoc = await UserModel.findOne({ username });
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+            // logged in
+            jwt.sign({ username, id: userDoc._id }, JWTsecret, {}, (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token, {
+                    httpOnly: true,
+                    // secure: process.env.NODE_ENV === 'production', // Set to true in production
+                    sameSite: 'None', // Add SameSite=None attribute
+                });
+                res.json({
+                    id: userDoc._id,
+                    username,
+                });
+            });
+        } else {
+            res.status(400).json('wrong credentials');
+        }
+    } catch (e) {
+        res.status(400).json("error");
     }
-
-  });
-
-router.post('/register', async (req,res) => {
-    const {username,password} = req.body;
-    const salt = bcrypt.genSaltSync(10)
-    try{
-      const userDoc = await UserModel.create({
-        username,
-        password:bcrypt.hashSync(password,salt),
-      });
-      res.json(userDoc);
-    } catch(e) {
-      console.log(e);
-      res.status(400).json(e);
-    }
-  });
-
-router.post('/logout', (req,res) => {
-    res.cookie('token', '').json('ok');
 });
 
-router.get('/profile', (req,res) => {
-    const {token} = req.cookies;
-    jwt.verify(token, JWTsecret, {}, (err,info) => {
-      if (err) {res.status(400).json("Couldn't get profile")};
-      res.json(info);
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    try {
+        const userDoc = await UserModel.create({
+            username,
+            password: bcrypt.hashSync(password, salt),
+        });
+        res.json(userDoc);
+    } catch (e) {
+        console.log(e);
+        res.status(400).json(e);
+    }
+});
+
+router.post('/logout', (req, res) => {
+    res.cookie('token', '', {
+        httpOnly: true,
+        // secure: process.env.NODE_ENV === 'production', // Set to true in production
+        sameSite: 'None', // Add SameSite=None attribute
+        expires: new Date(0), // Set expiration date in the past to delete the cookie
+    });
+    res.json('ok');
+});
+
+router.get('/profile', (req, res) => {
+    const { token } = req.cookies;
+    jwt.verify(token, JWTsecret, {}, (err, info) => {
+        if (err) {
+            res.status(400).json("Couldn't get profile");
+        } else {
+            res.json(info);
+        }
     });
 });
 
-module.exports = router
+module.exports = router;
